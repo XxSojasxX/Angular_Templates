c83d95be837d6ba0461275eae7cdb60e89b10558
Sebas <sojasvaca@gmail.com>
2023-05-29 19:06:04 -0500

    Angular_Templates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular_Prueba'`, () => {
+  it(`should have as title 'cliente'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular_Prueba');
+    expect(app.title).toEqual('cliente');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular_Prueba app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cliente app is running!');
   });
 });
